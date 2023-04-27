2099ccd8311eac7d78d6bdd3aa21aa36fe994876
Leopold Wieser <leopoldwieser1@gmail.com>
2023-04-27 20:06:36 +0200

    async processing

--- AI-project-issue-generator.py
+++ AI-project-issue-generator.py
@@ -4,6 +4,7 @@ import openai
 import dotenv
 import ast
 import argparse
+import asyncio
 
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -15,7 +16,8 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def get_chat_gpt_response(messages, model="gpt-3.5-turbo"):
     response = openai.ChatCompletion.create(
         model=model,
-        messages=messages)
+        messages=messages,
+        temperature=0.1)
     text = response['choices'][0]['message']["content"].strip()  # get the generated text
     return text
 
@@ -30,8 +32,11 @@ def create_message_from_text(text, role: str):
     }
 
 
-def generate_project_issue(idea, project_num):
+async def generate_project_issue(idea, project_num, task_context=''):
+    print(f"Generating issue for idea: {idea}")
     user_prompt = f"Generate a project task for the following idea:\n\n{idea}\n\n"
+    if task_context:
+        user_prompt += f"Context: {task_context}\n\n"
     system_prompt = """The task should be in the following format:
     {
         "title": "Task title",
@@ -42,29 +47,33 @@ def generate_project_issue(idea, project_num):
     The body should be a more detailed description of the issue. It should also include a user story, if possible.
     Dont forget to include quotes around the title and body.
     The body should be nicely formatted using markdown.
+    
     """
 
     messages = [create_message_from_text(user_prompt, "user"), create_message_from_text(system_prompt, "system")]
     issue_dict_str = get_chat_gpt_response(messages)
     issue_dict = ast.literal_eval(issue_dict_str)
 
-    print(f'Title: {issue_dict["title"]}\nBody: {issue_dict["body"]}')
-    return add_draft_item_to_project(project_num, issue_dict["title"], issue_dict["body"])
+    # print(f'Title: {issue_dict["title"]}\nBody: {issue_dict["body"]}')
+    return await add_draft_item_to_project(project_num, issue_dict["title"], issue_dict["body"])
+
+
+async def main():
+    project_num = input("Enter your Project Number (it shows up in the url of your project page): ")
+    task_context = input("Enter the context of your task (optional): ")
+    create_new = True
+
+    loop = asyncio.get_running_loop()  # Get the event loop
+
+    while create_new:
+        user_idea = await asyncio.get_event_loop().run_in_executor(None, input, "Enter your Task: ")
+        user_idea = user_idea.strip()
+        if user_idea == "exit":  # exit the program
+            create_new = False
+        else:
+            # generate the issue in a new task
+            loop.create_task(generate_project_issue(user_idea, project_num, task_context=task_context))
 
 
 if __name__ == "__main__":
-
-    # parse arguments
-    parser = argparse.ArgumentParser(description='Generate a project issue from an idea')
-    parser.add_argument('idea', type=str, help='The idea to generate an issue from')
-    # optional arguments
-    parser.add_argument('-p', '--project', type=int, help='The project number to add the issue to')
-    args = parser.parse_args()
-
-    # get project number
-    if args.project:
-        project_num = args.project
-    else:
-        project_num = 1  # get first project by default
-
-    generate_project_issue(args.idea, project_num)
+    asyncio.run(main())
--- github_api.py
+++ github_api.py
@@ -1,7 +1,10 @@
+import asyncio
+
 import requests
 import json
 import dotenv
 import os
+import aiohttp
 
 # import ENV variables from .env file
 dotenv.load_dotenv()
@@ -73,7 +76,43 @@ def create_project_card(note: str, column_id: int = 1):
 # get column id
 
 
-def graphql_request(query: str):
+# def graphql_request(query: str):
+#     data = {
+#         "query": query
+#     }
+#
+#     # Convert the data to a JSON string
+#     json_data = json.dumps(data)
+#
+#     # Set up the headers for the API request
+#     headers = {
+#         "Authorization": f"Token {access_token}",
+#         "Accept": "application/vnd.github.v3+json"
+#     }
+#
+#     # Make the API request to create the issue
+#     url = f"https://api.github.com/graphql"
+#     response = requests.post(url, headers=headers, data=json_data)
+#     if response.status_code == 200:
+#         print("GraphQL request successful!")
+#         print(response.json())
+#     return response
+#
+#
+# def get_project_id(project_num: int):
+#     res = graphql_request(f"query {{user(login: \"{owner}\") {{projectV2(number: {project_num}){{id}}}}}}")
+#     return res.json()["data"]["user"]["projectV2"]["id"]
+#
+#
+# def add_draft_item_to_project(project_num: int, title: str, body: str | None = None):
+#     project_id = get_project_id(project_num)
+#
+#     query = f"mutation {{addProjectV2DraftIssue(input: {{projectId: \"{project_id}\" title: \"{title}\" body: \"{body}\"}}) {{projectItem {{id}} }} }}"
+#
+#     return graphql_request(query)
+
+
+async def graphql_request(query: str, access_token: str):
     data = {
         "query": query
     }
@@ -87,23 +126,30 @@ def graphql_request(query: str):
         "Accept": "application/vnd.github.v3+json"
     }
 
-    # Make the API request to create the issue
-    url = f"https://api.github.com/graphql"
-    response = requests.post(url, headers=headers, data=json_data)
-    if response.status_code == 200:
-        print("GraphQL request successful!")
-        print(response.json())
-    return response
+    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=60)) as session:
+
+        # Make the API request to create the issue
+        url = f"https://api.github.com/graphql"
+        async with session.post(url, headers=headers, data=json_data) as resp:
+            if resp.status == 200:
+                await resp.json()
+                return resp
+            else:
+                print(resp.status)
+                print(await resp.json())
+                return resp
 
 
-def get_project_id(project_num: int):
-    res = graphql_request(f"query {{user(login: \"{owner}\") {{projectV2(number: {project_num}){{id}}}}}}")
-    return res.json()["data"]["user"]["projectV2"]["id"]
+async def get_project_id(project_num: int):
+    query = f"query {{user(login: \"{owner}\") {{projectV2(number: {project_num}){{id}}}}}}"
+    res = await graphql_request(query, access_token)
+    res_data = await res.json()
+    return res_data["data"]["user"]["projectV2"]["id"]
 
 
-def add_draft_item_to_project(project_num: int, title: str, body: str | None = None):
-    project_id = get_project_id(project_num)
+async def add_draft_item_to_project(project_num: int, title: str, body: str | None = None):
+    project_id = await get_project_id(project_num)
 
     query = f"mutation {{addProjectV2DraftIssue(input: {{projectId: \"{project_id}\" title: \"{title}\" body: \"{body}\"}}) {{projectItem {{id}} }} }}"
 
-    return graphql_request(query)
+    return await graphql_request(query, access_token)
